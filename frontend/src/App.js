942c42f1d14141ffbccb3129d6fba0e08a896daf
Jordan Boudreau <jordboudreau@gmail.com>
2019-01-20 18:30:08 -0800

    link form to post api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ class TodoList extends Component {
     this.state = {
       todos: []
     }
+
+    this.addTodo = this.addTodo.bind(this);
   }
 
   componentWillMount(){
@@ -33,6 +35,31 @@ class TodoList extends Component {
     .then(todos => this.setState({todos}));
   }
 
+  addTodo(todo){
+    fetch(APIURL, {
+      method: 'post',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({name: todo})
+    })
+    .then(response => {
+      if(!response.ok) {
+        if(response.status >= 400 && response.status < 500){
+          return response.json().then(data => {
+            let err = {errorMessage: data.message};
+            throw err;
+          })
+        } else {
+          let err = {errorMessage: 'Server not responding'};
+          throw err;
+        }
+      }
+      return response.json()
+    })
+    .then(todo => this.setState({todos: [...this.state.todos, todo]}));
+  }
+
   render() {
     let {todos} = this.state;
     let todoList = todos.map(todo => (
@@ -41,7 +68,7 @@ class TodoList extends Component {
     return (
       <div className="todoList">
         <h2>todolist</h2>
-        <TodoForm/>
+        <TodoForm addTodo={this.addTodo}/>
         <ul>
         {todoList}
         </ul>
@@ -51,11 +78,30 @@ class TodoList extends Component {
 }
 
 class TodoForm extends Component{
+  constructor(props){
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.props.addTodo(this.state.value)
+    this.setState({value: ''})
+  }
+
   render(){
     return (
-      <form>
-        <input type="text" placeholder="Insert your task here..." name="firstname"></input>
-      </form>
+      <div>
+        <input type="text" placeholder="Insert your task here..." name="todoInput" value={this.state.value} onChange={this.handleChange}/>
+        <button type="submit" onClick={this.handleSubmit}>Submit</button>
+      </div>
     )
   }
 }
